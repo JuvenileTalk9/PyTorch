a5d8e5a5003bd1aecb6c8f53caa79ad139fbffa0
JuvenileTalk9 <prdmq578@gmail.com>
2020-09-27 22:37:05 +0900

    03_Tensorによる演算

--- 03_Tensorによる演算/grad_sample.py
+++ 03_Tensorによる演算/grad_sample.py
@@ -1,14 +1,21 @@
 import torch
 
 def f(x):
-    return 2 * x * x + 5
+    """最小解を求めたい関数"""
+    return (x[0] - 2 * x[1] - 1)**2 +(x[1] * x[2] - 1)**2 + 1
 
 def f_grad(x):
-    g1 = 4 * x
-    return torch.Tensor([g1])
+    """変数がある値のときの微分値を求める関数"""
+    z = f(x)
+    z.backward()
+    return x.grad
 
 if __name__ == '__main__':
-    x = torch.Tensor([100])
+    # 初期値を決める（1, 2, 3）
+    x = torch.tensor([1., 2., 3.], requires_grad=True)
     for i in range(50):
+        # 微分値を求める
         x = x - 0.1 * f_grad(x)
-        print('x = {},  f = {}'.format(x, f(x)))
+        # 微分値に応じて少し移動
+        x = x.detach().requires_grad_(True)
+        print('x={},  f={}'.format(x.data, f(x).item()))
